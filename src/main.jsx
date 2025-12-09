e46253ebb0a54fe466d753d5b3c532ce1e30c047
w4h4jx87 <wahajwahaj200@gmail.com>
2025-12-10 00:43:23 +0500

    k72

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Stair from './Components/common/stair.jsx'
 import NavContext from './context/NavContext.jsx'
+import Stair from './components/layouts/Stair.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
